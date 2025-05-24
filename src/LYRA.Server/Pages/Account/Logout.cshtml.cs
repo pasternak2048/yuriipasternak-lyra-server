4005dfc8749ada697d0894f40adae13e584fb050
pasternak2048 <10pka.muscatel@gmail.com>
2025-05-24 06:04:10 +0300

    [LYRA.Server]: added logout

---
+++ src/LYRA.Server/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,28 @@
+using LYRA.Server.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LYRA.Server.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            return RedirectToPage("/Account/Login");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login");
+        }
+    }
+}
