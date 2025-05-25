7c0e56184c32462101cfc688beaee92dc80b8cb7
pasternak2048 <10pka.muscatel@gmail.com>
2025-05-25 05:18:57 +0300

    [LYRA.Server]: added ITrustedAgentService

---
+++ src/LYRA.Server/Models/Agents/TrustedAgentFilters.cs
@@ -0,0 +1,11 @@
+﻿using LYRA.Server.Models.Pagination;
+
+namespace LYRA.Server.Models.Agents
+{
+    public class TrustedAgentFilters : PaginationFilter
+    {
+        public string? Name { get; set; }
+
+        public Guid? CompanyId { get; set; }
+    }
+}
--- src/LYRA.Server/Program.cs
+++ src/LYRA.Server/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 
 builder.Services.AddScoped<IIdentityService, IdentityService>();
 builder.Services.AddScoped<ICompanyService, CompanyService>();
+builder.Services.AddScoped<ITrustedAgentService, TrustedAgentService>();
 
 builder.Services.AddRazorPages();
 
