e5e74171ca0284278846314b78c6908ffc4560a3
pasternak2048 <10pka.muscatel@gmail.com>
2025-08-19 20:19:32 +0300

    [LYRA][Server]: DI unification

---
+++ src/LYRA.Server/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace LYRA.Server.Extensions
+{
+	public static class WebApplicationBuilderExtensions
+	{
+		public static WebApplicationBuilder AddLyraApplication(this WebApplicationBuilder builder)
+		{
+			var configuration = builder.Configuration;
+			var services = builder.Services;
+
+			services.AddLyraDbContexts(configuration);
+			services.AddLyraMilanoCache(configuration);
+			services.AddLyraIdentity();
+			services.AddLyraAuthentication();
+			services.AddLyraCoreServices();
+			services.AddLyraWebApi();
+			services.AddLyraRealTime();
+			services.AddRazorPages();
+
+			return builder;
+		}
+	}
+}
